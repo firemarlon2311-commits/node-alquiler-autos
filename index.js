63e92e4d60a52442378d8ea60a5ff5901950925a
Marlon Gomez <firemarlon2311@gmail.com>
2026-03-30 22:31:41 -0500

    ultima

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import clientesRoutes from "./routes/clientesRoutes.js";
 import alquilerRoutes from "./routes/alquilerRoutes.js";
+import autosRoutes from "./routes/autosRoutes.js";
 
 const app = express();
 
@@ -9,11 +10,13 @@ app.use(express.json());
 app.use("/api", clientesRoutes);
 app.use("/api", alquilerRoutes);
 
+app.use("/api", autosRoutes);
+
 app.get("/", (req, res) => {
     res.send("API funcionando");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6001;
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto " + PORT);
