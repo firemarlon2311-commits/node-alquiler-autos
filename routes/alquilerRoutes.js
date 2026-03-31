bcdbb8beec5296e3abcdbf12393330d4e56a4388
Marlon Gomez <firemarlon2311@gmail.com>
2026-03-30 23:23:25 -0500

    jum

--- routes/alquilerRoutes.js
+++ routes/alquilerRoutes.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const router = express.Router();
-const alquilerController = require('../controller/alquilerController');
-
-// Ruta para registrar un nuevo alquiler
-router.post('/', alquilerController.realizarAlquiler);
-
-// Ruta para obtener el historial de alquileres
-router.get('/', alquilerController.historial);
-
-module.exports = router;
+router.get('/', (req, res) => {
+    res.send("ALQUILER FUNCIONANDO");
+});
